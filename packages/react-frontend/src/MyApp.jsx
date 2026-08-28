98fc47a5f7e62f73a35b3a2ee3bc1adce49fa234
timothykent38 <tikent@calpoly.edu>
2026-08-28 13:24:08 -0700

    commit for part 9 of IE1

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -2,27 +2,11 @@
 //import React from "react";
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-    name: "Charlie",
-    job: "Janitor",
-    },
-    {
-        name: "Mac",
-        job: "Bouncer",
-    },
-    {
-        name: "Dee",
-        job: "Aspring actress",
-    },
-    {
-        name: "Dennis",
-        job: "Bartender",
-    },
-  ]);
-
+  const [characters, setCharacters] = useState([]);
+    
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -33,6 +17,7 @@ function MyApp() {
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form />
     </div>
   );
 }
