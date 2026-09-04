f353907eef3e3c447d01c0dd07d6b9d5fad1e886
timothykent38 <tikent@calpoly.edu>
2026-09-03 22:32:25 -0700

    all done with IE2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,6 +38,43 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
+
+app.get("/users/:job/:name", (req, res) => {
+  const job = req.params.job; //or req.params.id
+  const name = req.params.name;
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    const result = users.users_list.filter(
+      (user) => user.name === name && user.job === job
+    );
+    res.send({ users_list: result });
+  }
+});
+
+// function to delete a user by their id
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  const result = users.users_list.findIndex((user) => user.id === id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    users.users_list.splice(result, 1)[0];
+  }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
