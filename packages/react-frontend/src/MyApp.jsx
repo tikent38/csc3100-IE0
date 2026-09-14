b773b8e84ed457e698e5da29073c4fafefe9522d
timothykent38 <tikent@calpoly.edu>
2026-09-14 11:03:46 -0700

    Part2 done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,6 +1,7 @@
 // src/MyApp.jsx
 //import React from "react";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import Table from "./Table";
 import Form from "./Form";
 
@@ -17,12 +18,28 @@ function MyApp() {
     setCharacters([...characters, person]);
   }
 
+  function fetchUsers() {
+  const promise = fetch("http://localhost:8000/users");
+  return promise;
+}
+useEffect(() => {
+  fetchUsers()
+    .then((res) => res.json())
+    .then((json) => setCharacters(json["users_list"]))
+    .catch((error) => {
+      console.log(error);
+    });
+}, []);
+
+
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
     </div>
   );
+
+  
 }
 
 export default MyApp;
