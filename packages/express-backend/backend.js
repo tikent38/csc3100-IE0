8d34bac9fadf0cc1b64d0d1a68c8bb6ea5eb9e74
timothykent38 <tikent@calpoly.edu>
2026-09-14 11:02:09 -0700

    IE3Part2 Done

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,9 +1,11 @@
 // backend.js
+import cors from "cors";
 import express from "express";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
